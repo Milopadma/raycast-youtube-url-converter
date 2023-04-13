7c903904e0ada71dc675aba736ad248c0ee9b8c1
mylo <milopadma@yahoo.com>
2023-04-13 20:16:23 +0800

    yt-dlp was not installed

--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,21 @@ import {
   List,
   OpenInBrowserAction,
   showToast,
+  Toast,
   ToastStyle,
 } from "@raycast/api";
 import { homedir } from "os";
 import { join } from "path";
 import youtubedl from "youtube-dl-exec";
-import createLogger from "progress-estimator";
+import ytdlp from "yt-dlp";
+import logger from "progress-estimator";
 
 export default function YoutubeURLList() {
   const items = [
     {
       title: "Convert YouTube Video to MP3",
       key: "convert",
-      url: "",
+      url: "https://www.youtube.com/watch?v=QH2-TGUlwu4",
     },
   ];
 
@@ -53,24 +55,12 @@ async function convertAndSave(url: string) {
   const outputDir = join(homedir(), "Downloads");
   const filename = `video_${Date.now()}.mp3`;
 
-  // use youtube-dl-exec
-  const promise = youtubedl("https://www.youtube.com/watch?v=6W_qHa1wq60", {
-    dumpSingleJson: true,
-  });
+  // use youtube-dl-exec to donwload the videos and use progress-estimator to show a progress bar
+  console.log(url);
+  await youtubedl(url, {
+    output: join(outputDir, filename),
+    noWarnings: true,
+  }).then((o) => console.log("Console", o, url));
 
-  // use progress-estimator
-  const estimate = createLogger({
-    storagePath: join(homedir(), ".progress-estimator"),
-  });
-
-  // show progress
-  const result = await estimate;
-  console.log(result);
-
-  showToast(ToastStyle.Animated, "Converting YouTube video to MP3...");
-
-  // conversion to complete, and show toast if success
-  // youtubeDl.then((output) => {
-  //   console.log(output);
-  // });
+  showToast(Toast.Style.Success, "YouTube video converted to MP3!");
 }
