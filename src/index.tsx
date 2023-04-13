fbb9da3af27b8e63f6dd2cc6a55e1f65088c15b0
mylo <milopadma@yahoo.com>
2023-04-13 21:26:09 +0800

    added support for url to mp4

--- src/index.tsx
+++ src/index.tsx
@@ -16,34 +16,40 @@ import logger from "progress-estimator";
 const youtubedl = create("/usr/local/bin/yt-dlp");
 
 export default function YoutubeURLList() {
+  let url = "";
   const items = [
     {
       title: "Convert YouTube Video to MP3",
-      key: "convert",
-      url: "https://www.youtube.com/watch?v=QH2-TGUlwu4",
+      key: "mp3",
+      url: "",
+    },
+    {
+      title: "Convert YouTube Video to MP4",
+      key: "mp4",
+      url: "",
     },
   ];
 
   return (
     <List
       searchBarPlaceholder="Enter YouTube URL"
-      onSearchTextChange={(text) => (items[0].url = text)}
+      onSearchTextChange={(text) => (url = text)}
       isLoading={items.length === 0}
       key={items[0].key}
     >
       {items.map((item) => (
         <List.Item
-          id={item.url}
+          id={url}
           title={item.title}
           actions={
             <ActionPanel>
               <ActionPanel.Item
                 title="Convert and Save"
                 icon={Icon.Download}
-                onAction={() => convertAndSave(item.url)}
+                onAction={() => convertAndSave(url, item.key)}
               />
-              <CopyToClipboardAction content={item.url} />
-              <OpenInBrowserAction url={item.url} />
+              <CopyToClipboardAction content={url} />
+              <OpenInBrowserAction url={url} />
             </ActionPanel>
           }
         />
@@ -52,21 +58,29 @@ export default function YoutubeURLList() {
   );
 }
 
-async function convertAndSave(url: string) {
+async function convertAndSave(url: string, key: string) {
   const outputDir = join(homedir(), "Downloads");
-  const filename = `video_${Date.now()}.mp3`;
+  const filename = `video_${Date.now()}.${key}`;
 
   // use youtube-dl-exec to donwload the videos and use progress-estimator to show a progress bar
-  console.log(url);
   try {
-    const output = await youtubedl(url, {
+    await youtubedl(url, {
       output: join(outputDir, filename),
-      extractAudio: true,
-      audioFormat: "mp3",
+      noCheckCertificates: true,
+      noWarnings: true,
+      preferFreeFormats: true,
+      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+      // if key is mp3, then extract the audio only
+      // if key is mp4, then download the video with audio
+      extractAudio: key === "mp3" ? true : false,
+      audioFormat: key === "mp3" ? "mp3" : "",
+      mergeOutputFormat: key === "mp4" ? "mp4" : undefined,
       ffmpegLocation: "/usr/local/bin/ffmpeg",
+      format: key === "mp4" ? "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" : undefined,
     });
-    showToast(Toast.Style.Success, "Downloaded " + filename + " from " + output.channel);
+    showToast(Toast.Style.Success, "Downloaded " + filename);
   } catch (error) {
-    showToast(Toast.Style.Failure, "Failed to convert YouTube video to MP3!" + error);
+    showToast(Toast.Style.Failure, "Failed" + error);
+    console.log(error);
   }
 }
